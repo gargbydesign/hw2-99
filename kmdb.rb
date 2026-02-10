ad3e7d870b109d296430184446bac9b806f82f43
gargbydesign <amit.garg@kellogg.northwestern.edu>
2026-02-10 02:52:34 +0000

    added movie and studio data to table

--- kmdb.rb
+++ kmdb.rb
@@ -98,8 +98,34 @@ Agent.destroy_all
 # Do not use hard-coded foreign key IDs.
 # TODO!
 
+## Studios
+studio = Studio.new
+studio["name"] = "Warner Bros."
+studio.save
 
+## Movies
+movie = Movie.new
+movie["title"] = "Batman Begins"
+movie["year_released"] = 2005
+movie["rated"] = "PG-13"
+movie["studio_id"] = Studio.find_by({"name" => "Warner Bros."})["id"]
+movie.save
 
+movie = Movie.new
+movie["title"] = "The Dark Knight"
+movie["year_released"] = 2008
+movie["rated"] = "PG-13"
+movie["studio_id"] = Studio.find_by({"name" => "Warner Bros."})["id"]
+movie.save
+
+movie = Movie.new
+movie["title"] = "The Dark Knight Rises"
+movie["year_released"] = 2012
+movie["rated"] = "PG-13"
+movie["studio_id"] = Studio.find_by({"name" => "Warner Bros."})["id"]
+movie.save
+
+ 
 # Prints a header for the movies output
 puts "Movies"
 puts "======"
