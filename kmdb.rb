beba7d2e11f016f253794031793ee4a7ff35ffeb
gargbydesign <amit.garg@kellogg.northwestern.edu>
2026-02-10 02:53:07 +0000

    added Actor data to db

--- kmdb.rb
+++ kmdb.rb
@@ -125,6 +125,54 @@ movie["rated"] = "PG-13"
 movie["studio_id"] = Studio.find_by({"name" => "Warner Bros."})["id"]
 movie.save
 
+
+## Actors
+actor = Actor.new
+actor["name"] = "Christian Bale"
+actor["agent_id"] = #???
+actor.save
+
+actor = Actor.new
+actor["name"] = "Michael Caine"
+actor.save
+
+actor = Actor.new
+actor["name"] = "Liam Neeson"
+actor.save
+
+actor = Actor.new
+actor["name"] = "Katie Holmes"
+actor.save
+
+actor = Actor.new
+actor["name"] = "Gary Oldman"
+actor.save
+
+actor = Actor.new
+actor["name"] = "Heath Ledger"
+actor.save
+
+actor = Actor.new
+actor["name"] = "Aaron Eckhart"
+actor.save
+
+actor = Actor.new
+actor["name"] = "Maggie Gyllenhaal"
+actor.save
+
+actor = Actor.new
+actor["name"] = "Tom Hardy"
+actor.save
+
+actor = Actor.new
+actor["name"] = "Joseph Gordon-Levitt"
+actor.save
+
+actor = Actor.new
+actor["name"] = "Anne Hathaway"
+actor.save
+
+
  
 # Prints a header for the movies output
 puts "Movies"
